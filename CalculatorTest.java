84e2ee0d93db173bf85ac6ff84bd0399c662f4a4
Tuan Bang <21012527@st.phenikaa-uni.edu.vn>
2024-04-21 16:19:08 +0700

    QC

---
+++ CalculatorTest.java
@@ -0,0 +1,23 @@
+package Qc;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    void testAdd() {
+    int a=4;
+    int b=6;
+        assertEquals(10, calculator.add(a, b));
+    }
+
+    @Test
+    void testSub() {
+    	int a=4;
+        int b=6;
+            assertEquals(-2, calculator.sub(a, b));
+    }
+
+}
